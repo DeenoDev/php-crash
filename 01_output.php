7144e61fa3bb179d5b3e58fca513cdf10534a2c4
DeenoDev <deyanoklaasen@outlook.com>
2023-11-12 23:58:41 +0200

    Used shorthand to echo PHP with ?= sign.

--- 01_output.php
+++ 01_output.php
@@ -16,7 +16,21 @@
 // var_dump([1, 2, 3]);
 
 //var_export() - Similar to var_dump(), Outputs a string representation of a variable.
-var_export('Hello');
+// var_export('Hello');
 
 // // Escaping characters with a backslash
 // echo "Is your name O\'reilly?";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= 'Post One'; ?></h1>
+</body>
+</html>
