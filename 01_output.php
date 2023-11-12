69ac1493390f76359990c3dbfee63a8ee7e7bb34
DeenoDev <deyanoklaasen@outlook.com>
2023-11-12 23:43:36 +0200

    echo statement completed.

--- 01_output.php
+++ 01_output.php
@@ -1,15 +1,4 @@
 <?php
 echo 'Hello123';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+echo 123, 'Hello', 10.5;
