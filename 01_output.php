0e76ddb06f3adc5125f89cdad7275fc6018fc750
DeenoDev <deyanoklaasen@outlook.com>
2023-11-12 23:52:10 +0200

    Added PHPView chrome extension and completed print_r extension.

--- 01_output.php
+++ 01_output.php
@@ -1,19 +1,19 @@
 <?php
-// Echo - Output strings, numbers, html, etc
-echo 'Hello';
-echo 123;
-echo '<h1>Hello</h1>';
+// // Echo - Output strings, numbers, html, etc
+// echo 'Hello';
+// echo 123;
+// echo '<h1>Hello</h1>';
 
-// print - Similar to echo, but a bit slower
-print 'Hello';
+// // print - Similar to echo, but a bit slower
+// print 'Hello';
 
 // print_r - Gives a bit more info. Can be used to print arrays
-print_r('Hello');
-print_r([1, 2, 3]);
+// print_r('Hello');
+// print_r([1, 2, 3]);
 
 // var_dump - Even more info like data type and length
 var_dump('Hello');
-var_dump([1, 2, 3]);
+// var_dump([1, 2, 3]);
 
-// Escaping characters with a backslash
-echo "Is your name O\'reilly?";
+// // Escaping characters with a backslash
+// echo "Is your name O\'reilly?";
