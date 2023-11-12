d0e9df8a72b24168792b91f609d2f744800cbdb2
DeenoDev <deyanoklaasen@outlook.com>
2023-11-12 23:45:37 +0200

    Added 01_ouput repo.

--- 01_output.php
+++ 01_output.php
@@ -1,4 +1,19 @@
 <?php
-echo 'Hello123';
+// Echo - Output strings, numbers, html, etc
+echo 'Hello';
+echo 123;
+echo '<h1>Hello</h1>';
 
-echo 123, 'Hello', 10.5;
+// print - Similar to echo, but a bit slower
+print 'Hello';
+
+// print_r - Gives a bit more info. Can be used to print arrays
+print_r('Hello');
+print_r([1, 2, 3]);
+
+// var_dump - Even more info like data type and length
+var_dump('Hello');
+var_dump([1, 2, 3]);
+
+// Escaping characters with a backslash
+echo "Is your name O\'reilly?";
