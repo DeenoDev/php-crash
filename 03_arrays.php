49c0e946b01e1054a6384fc18336aa6d7e975635
DeenoDev <deyanoklaasen@outlook.com>
2023-11-13 23:26:11 +0200

    standard associative array completed.

--- 03_arrays.php
+++ 03_arrays.php
@@ -4,4 +4,13 @@ $numbers = [1,44,55,22];
 $fruits = array('apple', 'orange', 'pear');
 
 // var_dump($numbers);
-echo $fruits[0];
+// echo $fruits[0];
+
+//Associative Array
+$colors = [
+  1 => 'red',
+  4 => 'blue',
+  6 => 'green'
+];
+
+echo $colors[4];
