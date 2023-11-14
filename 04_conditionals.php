a182e8bc326eb456712591703060a16a0a60b050
DeenoDev <deyanoklaasen@outlook.com>
2023-11-14 23:18:03 +0200

    Completed if statement conditionals using the >= operator.

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -35,63 +35,63 @@ if ($age >= 18) {
 // Dates
 // $today = date("F j, Y, g:i a");
 
-$t = date('H');
+// $t = date('H');
 
-if ($t < 12) {
-  echo 'Have a good morning!';
-} elseif ($t < 17) {
-  echo 'Have a good afternoon!';
-} else {
-  echo 'Have a good evening!';
-}
+// if ($t < 12) {
+//   echo 'Have a good morning!';
+// } elseif ($t < 17) {
+//   echo 'Have a good afternoon!';
+// } else {
+//   echo 'Have a good evening!';
+// }
 
-// Check if an array is empty
-// The isset() function will generate a warning or e-notice when the variable does not exists. The empty() function will not generate any warning or e-notice when the variable does not exists.
+// // Check if an array is empty
+// // The isset() function will generate a warning or e-notice when the variable does not exists. The empty() function will not generate any warning or e-notice when the variable does not exists.
 
-$posts = [];
+// $posts = [];
 
-if (!empty($posts[0])) {
-  echo $posts[0];
-} else {
-  echo 'There are no posts';
-}
+// if (!empty($posts[0])) {
+//   echo $posts[0];
+// } else {
+//   echo 'There are no posts';
+// }
 
-/* -------- Ternary Operator -------- */
-/*
-  The ternary operator is a shorthand if statement.
-  Ternary Syntax:
-    condition ? true : false;
-*/
+// /* -------- Ternary Operator -------- */
+// /*
+//   The ternary operator is a shorthand if statement.
+//   Ternary Syntax:
+//     condition ? true : false;
+// */
 
-// Echo based on a condition (Same as above)
-echo !empty($posts[0]) ? $posts[0] : 'There are no posts';
+// // Echo based on a condition (Same as above)
+// echo !empty($posts[0]) ? $posts[0] : 'There are no posts';
 
-// Assign a variable based on a condition
-$firstPost = !empty($posts[0]) ? $posts[0] : 'There are no posts';
+// // Assign a variable based on a condition
+// $firstPost = !empty($posts[0]) ? $posts[0] : 'There are no posts';
 
-$firstPost = !empty($posts[0]) ? $posts[0] : null;
+// $firstPost = !empty($posts[0]) ? $posts[0] : null;
 
-// Null Coalescing Operator ?? (PHP 7.4)
-// Will return null if $posts is empty
-// Always returns first parameter, unless first parameter happens to be NULL
-$firstPost = $posts[0] ?? null;
+// // Null Coalescing Operator ?? (PHP 7.4)
+// // Will return null if $posts is empty
+// // Always returns first parameter, unless first parameter happens to be NULL
+// $firstPost = $posts[0] ?? null;
 
-var_dump($firstPost);
+// var_dump($firstPost);
 
-/* -------- Switch Statements ------- */
+// /* -------- Switch Statements ------- */
 
-$favcolor = 'red';
+// $favcolor = 'red';
 
-switch ($favcolor) {
-  case 'red':
-    echo 'Your favorite color is red!';
-    break;
-  case 'blue':
-    echo 'Your favorite color is blue!';
-    break;
-  case 'green':
-    echo 'Your favorite color is green!';
-    break;
-  default:
-    echo 'Your favorite color is not red, blue, nor green!';
-}
+// switch ($favcolor) {
+//   case 'red':
+//     echo 'Your favorite color is red!';
+//     break;
+//   case 'blue':
+//     echo 'Your favorite color is blue!';
+//     break;
+//   case 'green':
+//     echo 'Your favorite color is green!';
+//     break;
+//   default:
+//     echo 'Your favorite color is not red, blue, nor green!';
+// }
