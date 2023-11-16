7aa60325a19d4d09953b4d43e34574d44268f59a
DeenoDev <deyanoklaasen@outlook.com>
2023-11-16 23:42:33 +0200

    for loop completed.

--- 05_loops.php
+++ 05_loops.php
@@ -15,72 +15,72 @@ for ($x = 0; $x <= 10; $x++) {
   echo "Number: $x <br>";
 }
 
-/* ------------ While Loop ------------ */
+// /* ------------ While Loop ------------ */
 
-/*
-** While Loop Syntax
-  while (condition) {
-  // code to be executed
-  }
-*/
+// /*
+// ** While Loop Syntax
+//   while (condition) {
+//   // code to be executed
+//   }
+// */
 
-$x = 1;
+// $x = 1;
 
-while ($x <= 5) {
-  echo "Number: $x <br>";
-  $x++;
-}
+// while ($x <= 5) {
+//   echo "Number: $x <br>";
+//   $x++;
+// }
 
-/* ---------- Do While Loop --------- */
+// /* ---------- Do While Loop --------- */
 
-/*
-** Do While Loop Syntax
-  do {
-  // code to be executed
-  } while (condition);
+// /*
+// ** Do While Loop Syntax
+//   do {
+//   // code to be executed
+//   } while (condition);
 
-do...while loop will always execute the block of code once, even if the condition is false.
-*/
+// do...while loop will always execute the block of code once, even if the condition is false.
+// */
 
-do {
-  echo "Number: $x <br>";
-  $x++;
-} while ($x <= 5);
+// do {
+//   echo "Number: $x <br>";
+//   $x++;
+// } while ($x <= 5);
 
-/* ---------- Foreach Loop ---------- */
+// /* ---------- Foreach Loop ---------- */
 
-/*
-** Foreach Loop Syntax
-  foreach ($array as $value) {
-  // code to be executed
-  }
-*/
+// /*
+// ** Foreach Loop Syntax
+//   foreach ($array as $value) {
+//   // code to be executed
+//   }
+// */
 
-// Loop through an array
+// // Loop through an array
 
-$numbers = [1, 2, 3, 4, 5];
+// $numbers = [1, 2, 3, 4, 5];
 
-foreach ($numbers as $x) {
-  echo "Number: $x <br>";
-}
+// foreach ($numbers as $x) {
+//   echo "Number: $x <br>";
+// }
 
-// Use the indexes within the loop
+// // Use the indexes within the loop
 
-$posts = ['Post One', 'Post Two', 'Post Three'];
+// $posts = ['Post One', 'Post Two', 'Post Three'];
 
-foreach ($posts as $index => $post) {
-  echo "${index} - ${post} <br>";
-}
+// foreach ($posts as $index => $post) {
+//   echo "${index} - ${post} <br>";
+// }
 
-// Use the keys within the loop for an associative array
+// // Use the keys within the loop for an associative array
 
-$person = [
-  'first_name' => 'Brad',
-  'last_name' => 'Traversy',
-  'email' => 'brad@gmail.com',
-];
+// $person = [
+//   'first_name' => 'Brad',
+//   'last_name' => 'Traversy',
+//   'email' => 'brad@gmail.com',
+// ];
 
-// Get Keys
-foreach ($person as $key => $val) {
-  echo "${key} - ${val} <br>";
-}
+// // Get Keys
+// foreach ($person as $key => $val) {
+//   echo "${key} - ${val} <br>";
+// }
