e9128f151ae2145e9fd1fd58a5006c899eafd1af
DeenoDev <deyanoklaasen@outlook.com>
2023-12-17 23:39:52 +0200

    created a User class.

--- 17_oop.php
+++ 17_oop.php
@@ -1,5 +1,15 @@
+<?php
+
+
 /* --- Object Oriented Programming -- */
 
 /*
   From PHP5 onwards you can write PHP in either a procedural or object oriented way. OOP consists of classes that can hold "properties" and "methods". Objects can be created from classes.
 */
+
+class User {
+    public $name;
+    public $email;
+    public $password;
+
+}
