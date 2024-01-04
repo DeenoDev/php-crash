7226e3fe9419ce53e9ec76c677a1c6d28f92d0e7
DeenoDev <deyanoklaasen@outlook.com>
2024-01-04 23:02:19 +0200

    used php include to include header into index.php file.

--- feedback/index.php
+++ feedback/index.php
@@ -1,41 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <title>Leave Feedback</title>
-</head>
-<body>
-  <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
-    <div class="container">
-      <a class="navbar-brand" href="#">OmniWeb</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-         <li class="nav-item">
-              <a class="nav-link" href="/php-crash/feedback/index.html">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/php-crash/feedback/feedback.html"
-                >Feedback</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/php-crash/feedback/about.html"
-                >About</a
-              >
-            </li>
-        </ul>
-      </div>
-  </div>
-</nav>
+<?php include 'inc/header.php'; ?>
 
-<main>
-  <div class="container d-flex flex-column align-items-center">
     <img src="/php-crash/feedback/img/logo.png" class="w-25 mb-3" alt="">
     <h2>Feedback</h2>
     <p class="lead text-center">Leave feedback for OmniWeb</p>
@@ -56,14 +20,3 @@
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
       </div>
     </form>
-    </div>
-</main>
-
-<footer class="text-center mt-5">
-  Copyright &copy; 2022
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
