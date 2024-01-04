dbf03929d94ca52cfc24a7442620a40b6d577cc4
DeenoDev <deyanoklaasen@outlook.com>
2024-01-04 23:10:12 +0200

    included footer into feedback.php file.

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -19,14 +19,4 @@
        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
      </div>
    </div>
-  </div>
-</main>
-
-<footer class="text-center mt-5">
-  Copyright &copy; 2022
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
+   <?php include 'inc/footer.php'; ?>
