67bbe5e8224405dbe199d864f7828f358531eaef
DeenoDev <deyanoklaasen@outlook.com>
2024-01-04 23:07:56 +0200

    included footer into about.php file.

--- feedback/about.php
+++ feedback/about.php
@@ -2,14 +2,4 @@
     <h2>About</h2>
 
     <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore impedit totam porro iure reiciendis autem possimus sapiente, optio, exercitationem ipsum assumenda mollitia, recusandae expedita culpa ratione voluptatem esse quos quam?</p>
-    </div>
-</main>
-
-<footer class="text-center mt-5">
-  Copyright &copy; 2022
-</footer>
- 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-</body>
-</html>
+<?php include 'inc/footer.php'; ?>
