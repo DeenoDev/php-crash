bc66a2aa81d2f0fd7f841050feac93071a8ad2d4
DeenoDev <deyanoklaasen@outlook.com>
2024-01-04 23:48:51 +0200

    created connection to database by using new mysqli function.

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -4,3 +4,6 @@
    define('DB_USER', 'deyano');
    define('DB_PASS', '123456');
    define('DB_NAME', 'php_dev');
+
+   //Create Connection
+   $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
