70690269b58a51e8c2ec142b700d6eec1d57faa0
DeenoDev <deyanoklaasen@outlook.com>
2024-01-06 16:08:09 +0200

    Added additional styling to feedback.html and wrote script to display name and body of feedback comment.

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -34,13 +34,15 @@
 
     <?php foreach($feedback as $item): ?>
 
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
+    <div class="card my-3 w-75">
+     <div class="card-body text-center">
+       <?php echo $item['body']; ?>
+       <div class="text-secondary mt2"></div>
+       By <?php echo $item['name']; ?>
      </div>
    </div>
 
-   <?php endforeach ?>
+   <?php endforeach; ?>
 
   
    
