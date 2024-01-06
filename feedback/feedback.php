9f459b13c32b2c19110988ba72d1786a0939aa06
DeenoDev <deyanoklaasen@outlook.com>
2024-01-06 16:01:58 +0200

    deleted 2 biolerplate card bodies.

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -38,16 +38,6 @@
      </div>
    </div>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
-
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+  
    
    <?php include 'inc/footer.php'; ?>
