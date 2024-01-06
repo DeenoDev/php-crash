239f1b5c00e27dce43e65977afb12f06d8636517
DeenoDev <deyanoklaasen@outlook.com>
2024-01-06 16:04:02 +0200

    created foreach loop in html feedback section.

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -32,11 +32,15 @@
        <p class="lead mt3">There is no feedback</p>
     <?php endif; ?>  
 
+    <?php foreach($feedback as $item): ?>
+
     <div class="card my-3">
      <div class="card-body">
        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
      </div>
    </div>
+
+   <?php endforeach ?>
 
   
    
